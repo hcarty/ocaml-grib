ad778bd75c3ee18cb023fb9590f124d3884642fb
Hezekiah M. Carty <hez@0ok.org>
2012-08-06 21:01:03 -0400

    Prepare for release 0.9.0, including G2clib

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 1f1be89ba627d38935b903ba811a0f4c) *)
+(* DO NOT EDIT (digest: 5d18a0e6f3280c5708e73a946a3197e6) *)
 module OASISGettext = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/oasis/OASISGettext.ml"
+# 21 "/home/mlavigne/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/oasis/OASISGettext.ml"
 
   let ns_ str =
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/oasis/OASISExpr.ml"
+# 21 "/home/mlavigne/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/oasis/OASISExpr.ml"
 
 
 
@@ -116,7 +116,7 @@ end
 
 # 117 "myocamlbuild.ml"
 module BaseEnvLight = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/base/BaseEnvLight.ml"
+# 21 "/home/mlavigne/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/base/BaseEnvLight.ml"
 
   module MapString = Map.Make(String)
 
@@ -214,7 +214,7 @@ end
 
 # 215 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml"
+# 21 "/home/mlavigne/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml"
 
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -323,7 +323,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-# 21 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+# 21 "/home/mlavigne/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
 
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -339,7 +339,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
 
-# 56 "/home/hcarty/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+# 56 "/home/mlavigne/ocamlbrew/ocaml-4.00.0/build/odb/install-oasis/oasis-0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
 
   type t =
       {
@@ -478,11 +478,38 @@ open Ocamlbuild_plugin;;
 let package_default =
   {
      MyOCamlbuildBase.lib_ocaml = [("grib", ["src"])];
-     lib_c = [("grib", "src", [])];
+     lib_c =
+       [
+          ("grib",
+            "src",
+            [
+               "src/g2clib-src/drstemplates.h";
+               "src/g2clib-src/gridtemplates.h";
+               "src/g2clib-src/grib2.h";
+               "src/g2clib-src/pdstemplates.h"
+            ])
+       ];
      flags =
        [
           (["oasis_library_grib_ccopt"; "compile"],
-            [(OASISExpr.EBool true, S [A "-ccopt"; A "-O2"])]);
+            [
+               (OASISExpr.EBool true, S [A "-ccopt"; A "-O2"]);
+               (OASISExpr.EFlag "png",
+                 S [A "-ccopt"; A "-O2"; A "-ccopt"; A "-DUSE_PNG"]);
+               (OASISExpr.EFlag "jpeg",
+                 S [A "-ccopt"; A "-O2"; A "-ccopt"; A "-DUSE_JPEG2000"]);
+               (OASISExpr.EAnd
+                  (OASISExpr.EFlag "jpeg", OASISExpr.EFlag "png"),
+                 S
+                   [
+                      A "-ccopt";
+                      A "-O2";
+                      A "-ccopt";
+                      A "-DUSE_JPEG2000";
+                      A "-ccopt";
+                      A "-DUSE_PNG"
+                   ])
+            ]);
           (["oasis_library_grib_cclib"; "link"],
             [
                (OASISExpr.EBool true,
@@ -795,6 +822,6 @@ let package_default =
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default package_default;;
 
-# 799 "myocamlbuild.ml"
+# 826 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
